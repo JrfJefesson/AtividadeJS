51e33ca50656ab36bd540c4d3db0a5978bc47129
Jéferson Ramos Fonseca <148767668+JrfJefesson@users.noreply.github.com>
2024-09-21 17:43:57 +0000

    att2

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function iniciarJogo() {
+    // Esconder os elementos atuais
+    document.getElementById('principal').style.display = 'none';
+
+    // Mostrar o novo conteúdo
+    document.getElementById('novo-conteudo').style.display = 'block';
+}
